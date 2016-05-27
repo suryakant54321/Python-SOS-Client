034a4cbc21af0c704b3950eb5834c55084726b8b
opengeo-vm <suryakant54321@gmail.com>
2016-05-27 18:47:20 +0530

    added example in overallflow for get capabilities and get observation request

--- src/overallFlow.py
+++ src/overallFlow.py
@@ -14,14 +14,41 @@
 #		Status: Complete
 #	To Do:
 #	5. Compose and push message on Telegram bot (teleCompose.py)
+		bot get SOS URL/s (NDBC, ISTSOS, SenseTube, etc.)
+		bot GetCapabilities
+		bot DescribeSensor
+		bot GetObservation (raw csv)
+		bot GetObservation (processed mean, max, min, median, sum, etc.)
+		bot GetObservation (graph)
 #
 # Improvement/s
 # 	7. Receive commands from user on Telegram bot
 #	8. Execute process from (1 to 5)
 #-----------------------------------------------------------------------
 """
+# 1. Example of Get Capabilities and Get Observation Request
+# GetObservation flow for new service
+import sosParseCap.py as pc
+import sosParseGetObs as go
+#
+help(pc)
+print(pc.url)
+pc.url = 'http://localhost/istsos/service?request=getCapabilities&section=contents&service=SOS'
+cap = pc.parseSOScap(pc.url)
+print(cap)
 
+# local url
+url = "http://localhost/istsos/service"
+go.startDate = '2012-11-30T20:30:11+0530'
+go.endDate = '2012-11-30T23:45:15+0530'
 
-
-
+go.ISTSOSrparams['eventTime']=go.startDate+'/'+go.endDate
+go.ISTSOSrparams['observedProperty']='urn:ogc:def:parameter:x-ksos:1.0:aws:station1:air_temperature'
+go.ISTSOSrparams['offering']='base_station_1'
+go.ISTSOSrparams['procedure']='temp_1'
+#
+msg, data = go.parseSOSgetObs(uurl, go.ISTSOSrparams, responseFormat='JSON')
+#
+print(msg)
+print(data)
 
